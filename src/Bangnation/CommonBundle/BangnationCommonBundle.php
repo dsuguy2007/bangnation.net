f7f20d1a57afedc9c80b04bd6e8b962cb0e2d363
wormling <wormling@wormtop>
2012-08-07 12:15:33 +0000

    Added and configured registration system

---
+++ src/Bangnation/CommonBundle/BangnationCommonBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Bangnation\CommonBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BangnationCommonBundle extends Bundle
+{
+}
---
+++ src/Bangnation/CommonBundle/Controller/WelcomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Bangnation\CommonBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class WelcomeController extends Controller
+{    
+    /**
+     * @Route("/", name="welcome")
+     * @Template()
+     */
+    public function indexAction() {        
+        return array(
+        );
+    }
+}
