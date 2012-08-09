166051a02912d3e534a128296f6b3a57ac3a7650
wormling <wormling@wormtop>
2012-08-09 21:43:52 +0000

    Added birthdate to registration form

---
+++ src/Bangnation/UserBundle/Form/Type/RegistrationFormType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Combo\UserBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
+
+class RegistrationFormType extends BaseType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        // add your custom field
+        $builder
+            ->add('birthDate')
+        ; 
+    }
+
+    public function getName()
+    {
+        return 'combo_user_registration';
+    }
+}
