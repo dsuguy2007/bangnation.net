7be7efa6af0afed55d06eeaa831dab3686fb1c6d
wormling <wormling@wormtop>
2012-08-09 21:36:37 +0000

    Added birthdate to user entity

--- src/Bangnation/UserBundle/Entity/User.php
+++ src/Bangnation/UserBundle/Entity/User.php
@@ -16,10 +16,48 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+    
+    /**
+     * @ORM\Column(type="birthday")
+     */
+    protected $birthDate;
 
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set birthDate
+     *
+     * @param birthday $birthDate
+     * @return User
+     */
+    public function setBirthDate(\birthday $birthDate)
+    {
+        $this->birthDate = $birthDate;
+    
+        return $this;
+    }
+
+    /**
+     * Get birthDate
+     *
+     * @return birthday 
+     */
+    public function getBirthDate()
+    {
+        return $this->birthDate;
     }
 }
