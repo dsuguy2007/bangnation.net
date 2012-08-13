4b5c1cf95ef3dca48afa389e9d70f2ee452663d4
wormling <wormling@wormtop>
2012-08-13 19:51:08 +0000

    Added ticker code and common javascript

---
+++ src/Bangnation/CommonBundle/Resources/public/js/common.js
@@ -0,0 +1,15 @@
+$(function() {
+    // Redirect on ajax calls to unauthorized pages (fixes expired session problem)
+    $('body').bind(
+        'ajaxError',
+        function(event, request, settings, error) {
+            console.debug(request.status);
+            if (request.status == '403') {
+                location.reload();
+            };
+        }
+    );
+        
+    $('#event-ticker').ticker();
+});
+
