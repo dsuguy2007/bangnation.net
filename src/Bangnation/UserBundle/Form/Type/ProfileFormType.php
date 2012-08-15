2c5caf73827170ce345120451783069772ed5445
wormling <wormling@wormtop>
2012-08-15 11:47:26 +0000

    Expanded turn ons/turn offs

--- src/Bangnation/UserBundle/Form/Type/ProfileFormType.php
+++ src/Bangnation/UserBundle/Form/Type/ProfileFormType.php
@@ -13,10 +13,22 @@ class ProfileFormType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('birthDate')
-            ->add('turnOns')
-            ->add('turnOffs')
+            ->add('birthDate', 'birthday', array(
+                'widget' => 'choice',
+                'empty_value' => array('year' => 'Year', 'month' => 'Month', 'day' => 'Day'),
+                'format' => 'yyyy-MMM-dd',                
+            ))
             ->add('profile', new ProfileType())
+            ->add('turnOns', null, array(
+                'label' => 'Turn Ons',
+                'multiple' => true,
+                'expanded' => true,
+            ))
+            ->add('turnOffs', null, array(
+                'label' => 'Turn Offs',
+                'multiple' => true,
+                'expanded' => true,
+            ))
         ;
     }
 
