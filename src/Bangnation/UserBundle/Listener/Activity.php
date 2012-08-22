e42c28061bfedc68a079f12b8642c27da1adc229
wormling <wormling@wormtop>
2012-08-22 22:06:21 +0000

    Activity listener added to update last_activity field in user bundle

---
+++ src/Bangnation/UserBundle/Listener/Activity.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Bangnation\UserBundle\Listener;
+
+use Symfony\Component\Security\Core\SecurityContext;
+use Doctrine\Bundle\DoctrineBundle\Registry as Doctrine;
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use DateTime;
+use Bangnation\UserBundle\Entity\User;
+
+class Activity
+{
+    protected $context;
+    
+    protected $em;
+    
+    public function __construct(SecurityContext $context, Doctrine $doctrine)
+    {
+        $this->context = $context;
+        $this->em = $doctrine->getEntityManager();
+    }
+    
+    /**
+     * On each request we want to update the user's last activity datetime
+     *
+     * @param \Symfony\Component\HttpKernel\Event\FilterControllerEvent $event
+     * @return void
+     */
+    public function onCoreController(FilterControllerEvent $event)
+    {
+        if ($this->context->getToken()) {
+            $user = $this->context->getToken()->getUser();
+            if($user instanceof User)
+            {
+                //here we can update the user as necessary
+                $user->setLastActivity(new DateTime());
+                $this->em->persist($user);
+                $this->em->flush($user);
+            }
+        }
+    }
+}
