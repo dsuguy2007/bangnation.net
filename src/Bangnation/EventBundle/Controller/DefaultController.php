7715d7f49f6335d38eba47a3167a7f2b16319d74
wormling <wormling@wormtop>
2012-08-22 20:35:07 +0000

    Base event templates

--- src/Bangnation/EventBundle/Controller/DefaultController.php
+++ src/Bangnation/EventBundle/Controller/DefaultController.php
@@ -3,17 +3,33 @@
 namespace Bangnation\EventBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/events", name="events")
      * @Template()
      */
-    public function indexAction($name)
+    public function eventsAction()
     {
-        return array('name' => $name);
+        $em = $this->getDoctrine()->getEntityManager();
+        $events = $em->getRepository('BangnationEventBundle:Event')->findAll();
+        
+        return array('events' => $events);
+    }
+    
+    /**
+     * @Route("/events/{slug}", name="event")
+     * @Template()
+     */
+    public function eventAction($slug)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $event = $em->getRepository('BangnationEventBundle:Event')->findOneBySlug($slug);
+        
+        return array('event' => $event);
     }
 }
