16d59144914e55b856ccbdcc120fc8bd1cf7dbcd
wormling <wormling@wormtop>
2012-08-23 20:43:11 +0000

    Created QR Code renderer

--- src/Bangnation/EventBundle/Controller/DefaultController.php
+++ src/Bangnation/EventBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Bangnation\EventBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -37,7 +38,8 @@ class DefaultController extends Controller
      * @Route("/event/calendar", name="event_calendar")
      * @Template()
      */
-    public function calendarAction() {        
+    public function calendarAction() 
+    {        
         $em = $this->getDoctrine()->getEntityManager();
         $events = $em->getRepository('BangnationEventBundle:Event')->findAll();
         
@@ -50,7 +52,8 @@ class DefaultController extends Controller
      * @Route("/eventdetail", name="event_detail")
      * @Template()
      */
-    public function eventDetailAction() {
+    public function eventDetailAction() 
+    {
         $id = $this->getRequest()->query->get('id');
 
         $em = $this->getDoctrine()->getEntityManager();
@@ -60,5 +63,33 @@ class DefaultController extends Controller
             'event' => $event, 
         );
     }
+    
+    /**
+     * Testing QRCode
+     * 
+     * @param string $code
+     * 
+     * @Route("/code/{code}", name="code")
+     */
+    public function renderQrCode($code)
+    {
+        $request = $this->getRequest();
+        
+        // Error correction
+        // Level L (Low)	7% of codewords can be restored.
+        // Level M (Medium)	15% of codewords can be restored.
+        // Level Q (Quartile)[28]	25% of codewords can be restored.
+        // Level H (High)	30% of codewords can be restored.
+        $ecl = $request->query->get('ecl', 'L');
+        $zoom - $request->query->get('zoom', '4');
+        $border = $request->query->get('border', 2);
 
+        include __DIR__.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."Resources".DIRECTORY_SEPARATOR."phpqrcode".DIRECTORY_SEPARATOR."qrlib.php";
+        
+        $response = new Response();
+        $response->headers->set('Content-Type', 'image/png');
+        $response->setContent(\QRcode::png($code, null, $ecl, $zoom, $border));
+        
+        return $response;
+    }
 }
