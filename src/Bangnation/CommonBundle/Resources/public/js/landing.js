695bd3b46095bd7ce8849664d3d53bd04ec695eb
wormling <wormling@wormtop>
2012-08-29 09:38:32 +0000

    XMPP Chat testing

--- src/Bangnation/CommonBundle/Resources/public/js/landing.js
+++ src/Bangnation/CommonBundle/Resources/public/js/landing.js
@@ -1,5 +1,6 @@
-function codeLatLng(lat, lng) {
 
+// Set the city/state in the registration form based on the users lat/lng
+function codeLatLng(lat, lng) {
     var latlng = new google.maps.LatLng(lat, lng);
     geocoder.geocode({
         'latLng': latlng
@@ -26,8 +27,8 @@ function codeLatLng(lat, lng) {
                         }
                     }
                 }
-                //city data
-                //alert(city.short_name + " " + city.long_name)
+            //city data
+            //alert(city.short_name + " " + city.long_name)
 
 
             } else {
@@ -42,6 +43,52 @@ function codeLatLng(lat, lng) {
 $(function() {
     $('#event-ticker').ticker();
    
+    $("#fos_user_registration_form_city").typeahead({
+        source: function(typeahead, query) {
+            var url = "http://ws.geonames.org/searchJSON";
+                
+            $.ajax({
+                dataType : 'jsonp',
+                data: {
+                    featureClass: "P",
+                    style: "full",
+                    maxRows: 12,
+                    name_startsWith: query
+                },
+                url: url,
+                success: function(data) {
+                    console.log(data);
+                    typeahead.process(data.geonames);
+                }
+            });
+        },
+        minLength: 2,
+        property: "adminName2"
+    });
+                
+    $("#fos_user_registration_form_state").typeahead({
+        source: function(typeahead, query) {
+            var url = "http://ws.geonames.org/searchJSON";
+                
+            $.ajax({
+                dataType : 'jsonp',
+                data: {
+                    featureClass: "P",
+                    style: "full",
+                    maxRows: 12,
+                    name_startsWith: query
+                },
+                url: url,
+                success: function(data) {
+                    console.log(data);
+                    typeahead.process(data.geonames);
+                }
+            });
+        },
+        minLength: 2,
+        property: "adminName1"
+    });
+                
     geocoder = new google.maps.Geocoder();
     //Check for geolocation support; hide "use my location" button if unsupported
     if (Modernizr.geolocation) { 
@@ -50,9 +97,8 @@ $(function() {
             var lng = position.coords.longitude;
             
             codeLatLng(lat, lng);
-        },function(){
-            alert('GeoLocation error');
         },
+        null,
         {
             timeout:10000
         });
