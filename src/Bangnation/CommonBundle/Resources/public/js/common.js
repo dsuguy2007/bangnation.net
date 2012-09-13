3c74f65f448d75dccb1e8b5fcfe6af22e3c67220
wormling <wormling@wormtop>
2012-09-13 18:50:36 +0000

    Ajax functionality for links

--- src/Bangnation/CommonBundle/Resources/public/js/common.js
+++ src/Bangnation/CommonBundle/Resources/public/js/common.js
@@ -9,5 +9,27 @@ $(function() {
             }
         }
     );
+        
+    $(document).on('click', '.ajax-link', function(e) {
+        e.preventDefault();
+
+        if(this.ajax_call) {
+            this.ajax_call.abort();
+        }
+
+        var url = $(this).attr('href');
+        var dataTarget = $(this).attr('data-content');
+
+        var _this = this;
+        _this.dataTarget = dataTarget;
+
+        this.ajax_call = $.ajax({
+            dataType : 'html',
+            url: url,
+            success: function(data) {
+                $(_this).closest(_this.dataTarget).replaceWith(data);
+            }
+        });
+    });
 });
 
